6377194d0e75c19e28f13f212f7ecaf8f0ab9d00
rhorner <rab.horner@tpicap.com>
2023-03-10 17:06:25 +0000

    PageNotFound added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import {Routes} from "react-router-dom";
-import {Route} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import AboutPage from "./about/AboutPage"
 import HomePage from "./home/HomePage";
 import Header from "../common/Header";
+import PageNotFound from "./PageNotFound";
 
 function App() {
     return (
@@ -12,8 +12,8 @@ function App() {
             <Routes>
                 <Route exact path="/" element={<HomePage/>} />
                 <Route path="/about" element={<AboutPage/>} />
+                <Route path="*" element={<PageNotFound/>} />
             </Routes>
-
         </div>
     )
 }
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const PageNotFound = () => <h1>Oh Oh Spaghettio, page is missing!</h1>
+
+export default PageNotFound;
